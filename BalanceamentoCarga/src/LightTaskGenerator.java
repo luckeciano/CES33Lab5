3eaa891d4e603454dfa9ec59e4c4a83875241ec8
Luckeciano Melo <luckeciano@hotmail.com>
2017-06-06 19:28:25 -0300

    initial commit

---
+++ BalanceamentoCarga/src/LightTaskGenerator.java
@@ -0,0 +1,18 @@
+
+public class LightTaskGenerator implements TaskGenerator {
+	private int TMT;
+	private int AMOUNT;
+	private int nProcessors;
+	private static int time = 0;
+	
+	public LightTaskGenerator (int tmt, int amount, int nProc) {
+		TMT = tmt;
+		AMOUNT = amount;
+		nProcessors = nProc;
+	}
+	
+	public Task generateTask(int TMT, int AMOUNT) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
